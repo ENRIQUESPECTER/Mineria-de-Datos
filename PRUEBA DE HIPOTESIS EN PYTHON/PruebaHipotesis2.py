acd83f847dd72d4525082c29893e96c39cf98ab3
ENRIQUESPECTER <luigienrique04@gmail.com>
2026-03-24 20:23:09 -0600

    Add files via upload

---
+++ PRUEBA DE HIPOTESIS EN PYTHON/PruebaHipotesis2.py
@@ -0,0 +1,40 @@
+#24 de Marzo del 2026
+#Bryan Hernández Flores
+#Luis Enrique Padilla Salmoran
+#Michael Rodriguez Resendiz
+     
+
+import numpy as np
+import scipy.stats as stats
+import pandas as pd
+     
+
+#Datos del problema
+mu_0 = 20 #Hipotesis nula
+n = 30 #Tamano de la prueba
+x_bar = 18.5 #Media muestral
+s = 2.5 #Desviacion estandar muestral
+alpha = 0.05 #Nivel de significancia
+     
+
+#Calculo del estadistico de prueba z
+z = (x_bar - mu_0) / (s/np.sqrt(n))
+     
+
+#Valor critico para la prueba
+z_critical = stats.norm.ppf(1-alpha)
+     
+
+#Comparacion de los valores
+if z > z_critical:
+    print("Rechazo la hipotesis nula")
+else:
+  print("No rechazar la hipotesis nula")
+  
+#Crear un dataframe con los datos
+df = pd.DataFrame({
+    'Parametros': ['Media Teorica', 'Media Muestral', 'Desviacion Estandar', 'Tamano de la muestra', 'Z-calculado', 'Z-critico', 'Conclusion'],
+    'valores': [mu_0, x_bar, s, n, z, z_critical, 'Rechazar la hipotesis nula' if z >z_critical else 'No rechazar la hipotesis nula']
+})
+
+print(df)
